7fcef064be35b5da06d3004c31e0e7a7c367688d
unknown <>
2016-07-16 23:47:30 +0900

    로그인화면,회원가입화면 이쁘게 꾸미기 // 글꼴all변경, 배치 깔끔하게

--- app/src/main/java/com/teamtoriden/photome/Activity/LoginActivity.java
+++ app/src/main/java/com/teamtoriden/photome/Activity/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.teamtoriden.photome.Activity;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
@@ -13,12 +12,15 @@ import android.widget.Toast;
 
 import com.teamtoriden.photome.R;
 
+import static android.graphics.Typeface.createFromAsset;
+
 public class LoginActivity extends AppCompatActivity {
 
     String myId;
     String myPassword;
     EditText idText;
     EditText pwText;
+    CheckBox box;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
         idText = (EditText) findViewById(R.id.idInput);
         pwText = (EditText) findViewById(R.id.pwInput);
+        box = (CheckBox) findViewById(R.id.checkBox);
 
         //비밀번호는 *모양으로 표시
         PasswordTransformationMethod passwdtm = new PasswordTransformationMethod();
@@ -37,15 +40,13 @@ public class LoginActivity extends AppCompatActivity {
 
         //saymael로 글꼴 변경
         Button button1 = (Button) findViewById(R.id.loginButton);
-        button1.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
-
+        button1.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
         Button button2 = (Button) findViewById(R.id.signupButton);
-        button2.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
-
-        CheckBox box = (CheckBox) findViewById(R.id.checkBox);
-        box.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
+        button2.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
+        box.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
+        idText.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
+        pwText.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
     }
-
 
     public boolean loginCheck(String id, String pw) {
         return idText.getText().toString().equals(id) && pwText.getText().toString().equals(pw);
@@ -53,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void LoginClicked(View view) {
         if (!loginCheck(myId, myPassword)) {
-            Toast.makeText(LoginActivity.this, "아이디와 비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginActivity.this, "로그인 정보가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
         } else {
             //MainActivity로 이동
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -63,7 +64,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void SignupClicked(View view) {
         //SignupActivity로 이동
-        Intent intent2 = new Intent(getApplicationContext(), SignupActivity.class);
-        startActivity(intent2);
+        Intent intent1 = new Intent(getApplicationContext(), SignupActivity.class);
+        startActivity(intent1);
     }
 }
--- app/src/main/java/com/teamtoriden/photome/Activity/SignupActivity.java
+++ app/src/main/java/com/teamtoriden/photome/Activity/SignupActivity.java
@@ -1,15 +1,15 @@
 package com.teamtoriden.photome.Activity;
 
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.teamtoriden.photome.R;
+
+import static android.graphics.Typeface.createFromAsset;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -32,16 +32,11 @@ public class SignupActivity extends AppCompatActivity {
 
         //saymael으로 폰트 변경
         EditText View1 = (EditText) findViewById(R.id.editUsername);
-        View1.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
-
-        EditText View2 = (EditText) findViewById(R.id.editPassword);
-        View2.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
-
-        EditText View3 = (EditText) findViewById(R.id.editConfirm);
-        View3.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
-
+        View1.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
+        Pw.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
+        confirmPw.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
         Button Button1 = (Button) findViewById(R.id.button);
-        Button1.setTypeface(Typeface.createFromAsset(getAssets(), "saymael.ttf"));
+        Button1.setTypeface(createFromAsset(getAssets(), "saymael.ttf"));
 
     }
 
@@ -51,7 +46,7 @@ public class SignupActivity extends AppCompatActivity {
             finish();
         } else {
             //패스워드가 일치하지 않을때 뜨는 경고메시지
-            Toast.makeText(SignupActivity.this, "The password you entered don't match", Toast.LENGTH_SHORT).show();
+            Toast.makeText(SignupActivity.this, "입력한 비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
         }
     }
 
